e697301934d46c7a7fb147d7e6dfc185f040a4f2
JUAN NIETO <Juanchosnieto@hotmail.com>
2024-09-12 09:19:15 -0500

    reservas v1.02

--- src/reservas.py
+++ src/reservas.py
@@ -3,11 +3,14 @@ def main():
     import random 
     from random import choice, randint
     num_aleatorio= random.choice(range(1,30))   
-    prefijo=input("¿cual prefijo lo define?, señor o señora: ")
+    prefijo = input("¿cual prefijo lo define?, señor o señora: ").upper()
+    while prefijo != "SEÑOR" or prefijo != "SEÑORA":
+        print("por favor elija entre, señor o señora: ") 
+        prefijo=input("").upper()
     nombre=input("por favor agregue su nombre y apellido: ")
     origen= int(input("seleccione el número de la ciudad de origen \n 1. Medellin \n 2. Bogotá \n 3. Cartagena \n")) 
     ciudad=origen
-    precio = 0
+    precio=0 
     if ciudad ==1:
         ciudad1= ("Medellín")
     elif ciudad ==2:
@@ -30,16 +33,16 @@ def main():
         destino1= ("Bogotá")
     elif destino ==3:
         destino1= ("Cartagena")
-    dia=input("Que dia desea viajar (lunes-domingo): ")
+    dia=input("Que dia desea viajar \n 1.lunes \n 2.martes \n 3.miercoles \n 4.jueves \n 5.viernes \n 6.sabado \n 7.domingo \n ")
     num_mes=int(input("seleccione el numero de ese dia (1-30):"))
     if origen==1 and destino==2 or origen==2 and destino==1 and dia in range (1,4):
-        precio= 79.900
+        precio=79900
     elif origen==1 and destino==2 or origen==2 and destino==1 and dia in range (5,7):
-        precio=119.900
-    if origen==1 and destino==3 or origen==3 and destino==1 and dia in range (1,4):
-        precio= 156.900
+        precio=119900
+    elif origen==1 and destino==3 or origen==3 and destino==1 and dia in range (1,4):
+        precio=156900
     elif origen==2 and destino==3 or origen==3 and destino==2 and dia in range (5,7):
-        precio= 213.000
+        precio=213000
     asiento=input("¿prefiere un asiento en: \n ventana \n pasillo \n sin preferencia \n ")
     if asiento == "ventana":
         num_asi=(f"{num_aleatorio}A")
@@ -48,7 +51,7 @@ def main():
     if asiento == "sin preferencia":
         num_asi=(f"{num_aleatorio}B")
 
-    print (f"Bienvenido {prefijo} {nombre} a cokeline airline \n su vuelo de {ciudad1} a {destino1} del dia {dia} {num_mes} de agosto está reservado. \n precio del voleto es de: {precio} \n su asiento es el {num_asi}")
+    print (f"Bienvenido a cokeline airline {prefijo} {nombre}.  \n su vuelo de {ciudad1} a {destino1} el dia {dia} {num_mes} de agosto está reservado. \n El precio del voleto es de: {precio} pesos \n Su asiento es el {num_asi}")
 
 
 if __name__ == "__main__":
--- src/satelite.py
+++ src/satelite.py
@@ -2,12 +2,15 @@
 def main():
     lift=int(input("agregue la altitud inicial del satélte (km): "))
     drag=float(input("agregue el coeficiente de arrastre: "))
-    alt_m=int(input("altura en la que el satelite se desintregra (km): "))
+    alt_m=int(input("altura en la que el satelite se desintregra el satélite (km): "))
+    contador= 0
+    print ("calculando las órbitas...")
     while drag<lift or lift<= alt_m:
         alt_perdida=drag*lift
         lift=lift-alt_perdida
         drag+=0.0001
-    print 
+        contador+= 1
+    print (f"el satélite ha ingresado a la atmosfera despues de {contador} órbitas")
 
 
 if __name__ == "__main__":
